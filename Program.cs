8d76a0b84624dc4fae80194306369c3b5660fa87
shes1996 <shes1996@bk.ru>
2022-07-16 22:20:01 +0400

    Программа на проверку выходного

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
-﻿Console.WriteLine("Введите число");
+﻿/*Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+/*Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine());
 int length = LengthOfNumber(number);
 if (length < 3) Console.WriteLine("Нет третьей цифры");
 else 
 {
     int third = (number / Convert.ToInt32(Math.Pow(10, (length - 3)))) % 10;
-    Console.WriteLine("Третья цифра "+ third);
+    Console.WriteLine("Третья цифра "+ Math.Abs(third));
 }
 
 
@@ -19,7 +24,16 @@ int LengthOfNumber(int number)
     }
     return i;
 }
-Console.WriteLine("/n");
-string num = Console.ReadLine();
-if (num[0] != '-') Console.WriteLine(num[2]);
-else Console.WriteLine(num[3]);
+*/
+/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+string[] days = {"Нет", "Нет", "Нет", "Нет", "Нет", "Да", "Да"};
+int day = Convert.ToInt32(Console.ReadLine());
+if (day <= 7 && day > 0)
+{
+    Console.WriteLine(days[day-1]);
+}
+else Console.WriteLine("Введено некорректное число.");
