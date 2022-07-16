fa56f24ac79fb05fecbbe9dfa3bed2756b095f8d
shes1996 <shes1996@bk.ru>
2022-07-16 21:41:04 +0400

    Программа нахождения третьего числа

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+int length = LengthOfNumber(number);
+if (length < 3) Console.WriteLine("Нет третьей цифры");
+else 
+{
+    int third = (number / Convert.ToInt32(Math.Pow(10, (length - 3)))) % 10;
+    Console.WriteLine("Третья цифра "+ third);
+}
+
+
+int LengthOfNumber(int number)
+{
+    int i = 0;
+    while (number != 0)
+    {
+        number /= 10;
+        i += 1;
+    }
+    return i;
+}
+Console.WriteLine("/n");
+string num = Console.ReadLine();
+if (num[0] != '-') Console.WriteLine(num[2]);
+else Console.WriteLine(num[3]);
