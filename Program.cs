1054dddcb175a797372874b9e679b7de4e426b4b
shes1996 <shes1996@bk.ru>
2022-07-16 22:21:59 +0400

    Программа нахождения третьей цифры в ветке задачи13

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 78 -> третьей цифры нет
 32679 -> 6*/
 
-/*Console.WriteLine("Введите число");
+Console.WriteLine("Введите число");
 int number = int.Parse(Console.ReadLine());
 int length = LengthOfNumber(number);
 if (length < 3) Console.WriteLine("Нет третьей цифры");
@@ -24,16 +24,3 @@ int LengthOfNumber(int number)
     }
     return i;
 }
-*/
-/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-6 -> да
-7 -> да
-1 -> нет*/
-
-string[] days = {"Нет", "Нет", "Нет", "Нет", "Нет", "Да", "Да"};
-int day = Convert.ToInt32(Console.ReadLine());
-if (day <= 7 && day > 0)
-{
-    Console.WriteLine(days[day-1]);
-}
-else Console.WriteLine("Введено некорректное число.");
